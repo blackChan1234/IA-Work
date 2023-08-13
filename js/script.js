3a91f5b257f96203fc16b9afa9e5897852fa0e9c
s7e7a <126228000+s7e7a@users.noreply.github.com>
2023-08-13 17:56:53 +0800

    fixed

--- js/script.js
+++ js/script.js
@@ -1,41 +1,79 @@
-angular.module('myApp', [])
-.controller('SliderController', function($scope, $interval) {
-    $scope.images = [
-        { src: "img/examplePic/image1.png", alt: "Image 1" },
-        { src: "img/examplePic/image2.png", alt: "Image 2" },
-        { src: "img/examplePic/image3.png", alt: "Image 3" },
-        { src: "img/examplePic/image4.png", alt: "Image 4" },
-        { src: "img/examplePic/image5.png", alt: "Image 5" },
-        { src: "img/examplePic/image6.png", alt: "Image 6" }
+document.addEventListener("DOMContentLoaded", () => {
+    const sliderMain = document.getElementById('sliderMain');
+    const prevBtn = document.getElementById('prev');
+    const nextBtn = document.getElementById('next');
+    const sliderIndex = document.getElementById('sliderIndex');
+
+    const images = [
+        "img/examplePic/image1.png",
+        "img/examplePic/image2.png",
+        "img/examplePic/image3.png",
+        "img/examplePic/image4.png",
+        "img/examplePic/image5.png",
+        "img/examplePic/image6.png"
     ];
 
-    $scope.currentIndex = 0;
-    $scope.autoLoopInterval = startAutoLoopInterval();
+    let currentIndex = 0;
+    
+    function createImageElement(src, alt) {
+        const img = document.createElement('img');
+        img.src = src;
+        img.alt = alt;
+        return img;
+    }
+
+    function updateIndexIndicator() {
+        const indexIndicators = sliderIndex.querySelectorAll('.index');
+        indexIndicators.forEach((indicator, index) => {
+            indicator.classList.toggle('currentIndex', index === currentIndex);
+        });
+    }
+
+    function slideTo(index) {
+        currentIndex = (index + images.length) % images.length;
+        sliderMain.style.transform = `translateX(${-currentIndex * 100}%)`;
+        updateIndexIndicator();
+    }
+
+    prevBtn.addEventListener('click', () => {
+        slideTo(currentIndex - 1);
+    });
+
+    nextBtn.addEventListener('click', () => {
+        slideTo(currentIndex + 1);
+    });
+
+    images.forEach((imageSrc, index) => {
+        const imageElement = createImageElement(imageSrc, `Image ${index + 1}`);
+        const item = document.createElement('div');
+        item.className = 'item';
+        const link = document.createElement('a');
+        link.href = '#';
+        link.appendChild(imageElement);
+        item.appendChild(link);
+        sliderMain.appendChild(item);
+
+        const indexIndicator = document.createElement('div');
+        indexIndicator.className = 'index';
+        indexIndicator.addEventListener('click', () => slideTo(index));
+        sliderIndex.appendChild(indexIndicator);
+    });
+
+    updateIndexIndicator();
+
+    let autoLoopInterval = startAutoLoopInterval();
 
     function startAutoLoopInterval() {
-        return $interval(function() {
-            $scope.slideTo($scope.currentIndex + 1);
+        return setInterval(() => {
+            slideTo(currentIndex + 1);
         }, 3000);
     }
 
     function resetAutoLoopInterval() {
-        $interval.cancel($scope.autoLoopInterval);
-        $scope.autoLoopInterval = startAutoLoopInterval();
+        clearInterval(autoLoopInterval);
+        autoLoopInterval = startAutoLoopInterval();
     }
 
-    $scope.slideTo = function(index) {
-        $scope.currentIndex = (index + $scope.images.length) % $scope.images.length;
-        $scope.updateIndexIndicator();
-        resetAutoLoopInterval();
-    };
-
-    $scope.updateIndexIndicator = function() {
-        angular.forEach($scope.images, function(image, index) {
-            image.current = (index === $scope.currentIndex);
-        });
-    };
-
-    $scope.$on('$destroy', function() {
-        $interval.cancel($scope.autoLoopInterval);
-    });
+    sliderMain.addEventListener('mouseenter', resetAutoLoopInterval);
+    sliderMain.addEventListener('mouseleave', resetAutoLoopInterval);
 });
