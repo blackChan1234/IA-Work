3223c23312b66ef91832d049cb289beeb83e4364
blackChan1234 <142002098+blackChan1234@users.noreply.github.com>
2023-08-13 12:57:33 +0800

    sql

---
+++ iadb.sql
@@ -0,0 +1,94 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- 主机： 127.0.0.1
+-- 生成日期： 2023-08-13 06:56:52
+-- 服务器版本： 10.4.27-MariaDB
+-- PHP 版本： 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 数据库： `iadb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `pdf`
+--
+
+CREATE TABLE `pdf` (
+  `pdfID` int(11) NOT NULL,
+  `fileName` varchar(200) NOT NULL,
+  `userName` varchar(200) NOT NULL,
+  `pdfDescription` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user`
+--
+
+CREATE TABLE `user` (
+  `userID` int(11) NOT NULL,
+  `Name` varchar(200) NOT NULL,
+  `password` int(11) NOT NULL,
+  `group` varchar(200) NOT NULL DEFAULT 'member'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 转存表中的数据 `user`
+--
+
+INSERT INTO `user` (`userID`, `Name`, `password`, `group`) VALUES
+(1, 'Steve Chan', 123, 'member'),
+(2, 'ABC', 123, 'member'),
+(3, 'GDJ', 255, '');
+
+--
+-- 转储表的索引
+--
+
+--
+-- 表的索引 `pdf`
+--
+ALTER TABLE `pdf`
+  ADD PRIMARY KEY (`pdfID`);
+
+--
+-- 表的索引 `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userID`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `pdf`
+--
+ALTER TABLE `pdf`
+  MODIFY `pdfID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- 使用表AUTO_INCREMENT `user`
+--
+ALTER TABLE `user`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
