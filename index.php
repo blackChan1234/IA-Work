e001fd4a5093364d9389ae016a12c19e9d7b1997
blackChan1234 <142002098+blackChan1234@users.noreply.github.com>
2023-08-15 13:32:27 +0800

    add new menu and change login style
    when user screen over 769px it will show other menu

---
+++ index.php
@@ -0,0 +1,138 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.8.2/angular.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style/bootstrap.min.css" />
+    <link rel="stylesheet" href="style\menu.css">
+    <link rel="stylesheet" href="style\btnGroup.css">
+    <link rel="stylesheet" href="style\logo.css">
+    <link rel="stylesheet" href="style\avatar.css">
+    <link rel="stylesheet" href="style\menuPicFrame.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" />
+    <link rel="stylesheet" href="style/dataTables.bootstrap5.min.css" />
+    <script src="js\HamburgerMenu.js"></script>
+    <script src="js\LinkController.js"></script>
+    <script src="./js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.0.2/dist/chart.min.js"></script>
+    <script src="./js/jquery-3.5.1.js"></script>
+    <script src="./js/jquery.dataTables.min.js"></script>
+    <script src="./js/dataTables.bootstrap5.min.js"></script>
+
+</head>
+<body>
+<!-- Menu -->
+<div ng-app="myApp" class="topnav">
+    <a href="#home" id="logo"></a>
+
+    <div ng-controller="LinkController">
+        <div id="Links">
+            <a ng-repeat="link in links" ng-href="{{link.href}}">{{link.text}}</a>
+        </div>
+    </div>
+    <a href="javascript:void(0);" class="icon" onclick="HamburgerMenu()">
+        <i class="fa fa-bars"></i>
+    </a>
+</div>
+
+<div id="slider" class="slider">
+    <div id="sliderScroll" class="sliderScroll">
+        <div id="sliderMain" class="sliderMain"></div>
+        <span id="prev" class="prev">&#10094;</span>
+        <span id="next" class="next">&#10095;</span>
+    </div>
+    <div id="sliderIndex" class="sliderIndex"></div>
+</div>
+<div class="desktop-content">
+<?php include 'menu.php'; ?>
+</div>
+
+    <script src="js\script.js"></script>
+
+    <div class="button-block">
+        <button class="buttongroup">Button 1</button>
+        <button class="buttongroup">Button 2</button>
+        <button class="buttongroup">Button 3</button>
+    </div>
+    <link rel="stylesheet" href="style\indexContent.css">
+    <div class="content">
+        <img src="img\example.png" alt="Image" class="float-left">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum. Suspendisse auctor
+            justo dui, a euismod velit convallis nec. Suspendisse id nulla non arcu fermentum facilisis. Nulla facilisi.
+            Mauris interdum ante eu luctus commodo. Fusce sed justo id justo faucibus commodo a id turpis.
+        </p>
+    </div>
+
+</body>
+=======
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.8.2/angular.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="style\menu.css">
+    <link rel="stylesheet" href="style\btnGroup.css">
+    <link rel="stylesheet" href="style\logo.css">
+    <link rel="stylesheet" href="style\avatar.css">
+    <link rel="stylesheet" href="style\menuPicFrame.css">
+    <script src="js\HamburgerMenu.js"></script>
+    <script src="js\LinkController.js"></script>
+
+</head>
+
+<body>
+
+    <!-- Menu -->
+    <div ng-app="myApp" class="topnav">
+            <a href="#home" id="logo"></a>
+
+        <div ng-controller="LinkController">
+            <div id="Links">
+                <a ng-repeat="link in links" ng-href="{{link.href}}">{{link.text}}</a>
+            </div>
+        </div>
+        <a href="javascript:void(0);" class="icon" onclick="HamburgerMenu()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div>
+
+    <div id="slider" class="slider">
+        <div id="sliderScroll" class="sliderScroll">
+            <div id="sliderMain" class="sliderMain"></div>
+            <span id="prev" class="prev">&#10094;</span>
+            <span id="next" class="next">&#10095;</span>
+        </div>
+        <div id="sliderIndex" class="sliderIndex"></div>
+    </div>
+    <script src="js\script.js"></script>
+
+    <div class="button-block">
+        <button class="buttongroup">Button 1</button>
+        <button class="buttongroup">Button 2</button>
+        <button class="buttongroup">Button 3</button>
+    </div>
+    <link rel="stylesheet" href="style\indexContent.css">
+    <div class="content">
+        <img src="img\example.png" alt="Image" class="float-left">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec odio ipsum. Suspendisse auctor
+            justo dui, a euismod velit convallis nec. Suspendisse id nulla non arcu fermentum facilisis. Nulla facilisi.
+            Mauris interdum ante eu luctus commodo. Fusce sed justo id justo faucibus commodo a id turpis.
+        </p>
+    </div>
+
+</body>
+
+</html>
---
+++ menu.php
@@ -0,0 +1,89 @@
+<?php
+session_start(); // Make sure to start the session if it's not started already
+?>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="offcanvasExample">
+            <span class="navbar-toggler-icon" data-bs-target="#sidebar"></span>
+        </button>
+        <a class="navbar-brand me-auto ms-lg-0 ms-3 text-uppercase fw-bold" href="index.php">Yummy Restaurant Group Limited</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#topNavBar" aria-controls="topNavBar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="topNavBar">
+            <form class="d-flex ms-auto my-3 my-lg-0">
+                <div class="input-group">
+
+
+                </div>
+            </form>
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle ms-2" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="bi bi-person-fill"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                        <li><a class="dropdown-item" href="#">Settings</a></li>
+                        <li>
+                            <a class="dropdown-item" href="logout.php">Logout</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- top navigation bar -->
+<!-- offcanvas -->
+<div class="offcanvas offcanvas-start sidebar-nav bg-dark" tabindex="-1" id="sidebar">
+    <div class="offcanvas-body p-0">
+        <nav class="navbar-dark">
+            <ul class="navbar-nav">
+
+                <li>
+                    <a href="index.php" class="nav-link px-3 active">
+                        <span class="me-2"><i class="bi bi-house"></i></span>
+                        <span>Home</span>
+                    </a>
+                </li>
+                <li class="my-4">
+                    <hr class="dropdown-divider bg-light" />
+                </li>
+                <li>
+                    <div class="text-muted small fw-bold text-uppercase px-3 mb-3">
+                        Interface
+                    </div>
+                </li>
+                <li>
+                    <?php if (isset($_SESSION['role']) && $_SESSION['role'] == 'Supplier'): ?>
+                    <a href="#" class="nav-link disabled px-3" id="Purchase">
+                        <?php else: ?></a>
+                    <a href="PurchaseSystem.php" class="nav-link px-3" id="Purchase">
+                        <?php endif; ?>
+                        <span class="me-2"><i class="bi bi-cart-fill"></i></span>
+                        <span>Purchase Management</span>
+                    </a>
+                </li>
+                <li>
+                    <?php if (isset($_SESSION['role']) && $_SESSION['role'] == 'Purchase Manager'): ?>
+                    <a href="#" class="nav-link disabled px-3" id="Supplier">
+                        <?php else: ?></a>
+                    <a href="Supplier.php" class="nav-link px-3" id="Supplier">
+                        <?php endif; ?>
+                        <span class="me-2"><i class="bi bi-truck"></i></span>
+                        <span>Supplier</span>
+
+                    </a>
+                </li>
+
+                <li class="my-4">
+                    <hr class="dropdown-divider bg-light" />
+                </li>
+
+
+            </ul>
+        </nav>
+    </div>
+</div>
+  
