20762960b4b7344a50a6e1378192dd8ca70c4f2b
s7e7a <126228000+s7e7a@users.noreply.github.com>
2023-08-17 15:18:42 +0800

    update news

---
+++ newsTest1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>News Website</title>
+    <link rel="stylesheet" href="style\news.css">
+    <script src="script\newsTest1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.8.2/angular.min.js"></script>
+    </head>
+<body>
+
+
+    <section class="news-list">
+        <article class="news-item">
+            <img src="img\example.png" alt="News Image">
+            <h2>Breaking News 1</h2>
+            <p>This is the first breaking news. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <div class="read-more">
+                <p>This is the extended content of the news article. It will be hidden initially.</p>
+            </div>
+            <div class="read-more-btn">Read More</div>
+        </article>
+
+    </section>
+
+    <footer>
+        <p>&copy; 2023 News Website. All rights reserved.</p>
+    </footer>
+</body>
+</html>
