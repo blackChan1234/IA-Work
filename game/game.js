cf337ad72e264a7a4f2e5c7300092378227c60d3
s7e7a <126228000+s7e7a@users.noreply.github.com>
2023-08-20 18:33:01 +0800

    Update game.js

--- game/game.js
+++ game/game.js
@@ -1,73 +1,81 @@
-function Game(gameHour,readHour){
-  if(!checkCookie("game_data")){
-    // new data
+function Game(gameHour, readHour) {
+  if (!checkCookie("game_data")) {
+    // create new game data
     var expirationDate = new Date();
     var data = {
-        gameTime: [gameHour],
-        readTime: [readHour],
-        nodata: [false],
-        startTime: expirationDate.getTime() // 假設為遊戲/閱讀的開始時間
+      gameTime: [gameHour],
+      readTime: [readHour],
+      startTime: expirationDate.getTime() // 假設為遊戲/閱讀的開始時間
     };
-    
+
     var jsonData = JSON.stringify(data);
-    
-    
+
+
     expirationDate.setDate(expirationDate.getDate() + 30);
-    
+
     // document.cookie = "game_data=" + jsonData + "; expires=" + expirationDate.toUTCString() + "; path=/";
-    setCookie("game_data",jsonData,expirationDate.toUTCString());
-    
-    // new data
+    setCookie("game_data", jsonData, expirationDate.toUTCString());
+
+    // create new game data
   }
-  else{
+  else {
     //read data
     var expirationDate = new Date();
-    var jsonData = getCookie("game_data");
-    const data = JSON.parse(jsonData);
-    console.log("Game Time:", data.gameTime);
-    console.log("Read Time:", data.readTime);
-    console.log("no data:", data.nodata);
-    console.log("Start Time:", new Date(data.startTime));
-    startTime = new Date(data.startTime);
+    var data = readGameData();
+    printCookie();
 
-    numOfdays =(new Date()).getDate()-startTime.getDate();
-    for (var i = (data.nodata).length; i < numOfdays-1;){
-      data.nodata.push(true);
-    }
-    data.nodata.push(false);
+    startTime = new Date(data.startTime);
+    numOfdays = (new Date()).getDate() - startTime.getDate();
     expirationDate.setDate(expirationDate.getDate() + 30);
-    
+
+    //full null data
+    for (var i = (data.readTime).length; i < numOfdays;) {
+      data.gameTime.push("null");
+      data.readTime.push("null");
+    }
+
+    data.gameTime[numOfdays] = gameHour;
+    data.readTime[numOfdays] = readHour;
+
 
     jsonData = JSON.stringify(data);
-    setCookie("game_data",jsonData,expirationDate.toUTCString());
+    setCookie("game_data", jsonData, expirationDate.toUTCString());
   }
 
 }
 
+function readGameData() {
+  var jsonData = getCookie("game_data");
+  return JSON.parse(jsonData);
+}
+function printCookie() {
+  var data = readGameData()
+  console.log("Game Time:", data.gameTime);
+  console.log("Read Time:", data.readTime);
+  console.log("Start Time:", new Date(data.startTime));
+}
 
 
-
-
-function setCookie(cname,cvalue,expires) {
-document.cookie = cname + "=" + cvalue + ";" +"expires=" + expires + ";path=/";
+function setCookie(cname, cvalue, expires) {
+  document.cookie = cname + "=" + cvalue + ";" + "expires=" + expires + ";path=/";
 }
 
 function getCookie(cname) {
-let name = cname + "=";
-let decodedCookie = decodeURIComponent(document.cookie);
-let ca = decodedCookie.split(';');
-for(let i = 0; i < ca.length; i++) {
-  let c = ca[i];
-  while (c.charAt(0) == ' ') {
-    c = c.substring(1);
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
   }
-  if (c.indexOf(name) == 0) {
-    return c.substring(name.length, c.length);
-  }
-}
-return "";
+  return "";
 }
 
 function checkCookie(cname) {
-return (getCookie(cname) != ""); 
+  return (getCookie(cname) != "");
 }
