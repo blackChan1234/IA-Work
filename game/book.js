7dea7b55165d826dc3339e85eabcf2ee662eecbb
s7e7a <126228000+s7e7a@users.noreply.github.com>
2023-08-23 11:25:22 +0800

    Update book.js

--- game/book.js
+++ game/book.js
@@ -1,16 +1,16 @@
 function getPageInfo(page) {
     try {
-    const pageInfo = pageData[page]; // 使用頁數索引獲取對應的數據
-    if (pageInfo) {
-        return pageInfo;
+        const pageInfo = pageData[page]; // 使用頁數索引獲取對應的數據
+        if (pageInfo) {
+            return pageInfo;
+        }
+    }
+    finally {
+        return { text: `page ${page + 1}` }; // 默認顏色和空文字
     }
 }
-finally{
-    return { text: `page ${page+1}` }; // 默認顏色和空文字
-}
-}
 
-function setPageData(page,date){
+function setPageData(page, date) {
     if (!checkCookie("Book")) {
         // create new game data
         var expirationDate = new Date();
@@ -18,19 +18,19 @@ function setPageData(page,date){
             pages: page,
             date: date
         };
-    
+
         var jsonData = JSON.stringify(Book);
-    
-    
+
+
         expirationDate.setDate(expirationDate.getDate() + 30);
-    
+
         // document.cookie = "game_data=" + jsonData + "; expires=" + expirationDate.toUTCString() + "; path=/";
         setCookie("Book", jsonData, expirationDate.toUTCString());
 
-      }
+    }
 }
 
-function getExpirationDate(){
+function getExpirationDate() {
     var expirationDate = new Date();
     return expirationDate.getDate();
 }
@@ -38,25 +38,35 @@ function getExpirationDate(){
 function readBookData() {
     var jsonData = getCookie("Book");
     return JSON.parse(jsonData);
-  }
-function aPage(){
+}
+function aPage() {
     const pageDiv = document.createElement('div');
-        pageDiv.className = 'hard';
+    pageDiv.className = 'hard';
     return pageDiv;
 
+}
+function newLine(elementOfParent, text) {
+    div = document.createElement('div');
+    div.textContent = text;
+    elementOfParent.append(div);
+}
+function addNewElement(elementOfParent, text,type) {
+    div = document.createElement(type);
+    div.textContent = text;
+    elementOfParent.append(div);
 }
 
 $(document).ready
     (function () {
-        day=0; 
+        day = 0;
         var data;
         if (checkCookie("game_data")) {
-        data = readGameData();
-        printGameCookie();
-        day=numOfdays(data);
+            data = readGameData();
+            printGameCookie();
+            day = numOfdays(data);
         }
-        
-        
+
+
         /*
         <div class="hard"> Turn.js </div> 
         <div class="hard"></div>
@@ -67,33 +77,45 @@ $(document).ready
         <div class="hard"></div>
         <div class="hard"></div>
         
-        */ 
-        pageLen=day;
+        */
+        pageLen = day;
         pageLen++;
-        bookTitle="T";
+        bookTitle = "T";
 
-        pageFirst=aPage();
-        pageFirst.textContent= bookTitle;
+        
+        
+        pageFirstTitle = document.createElement("h2");
+        pageFirstTitle.textContent = bookTitle;
+        
+        pageFirst = aPage();
+        pageFirst.append(pageFirstTitle);
+
         $("#flipbook").append(pageFirst);
         $("#flipbook").append(aPage());
-        
+
         for (let i = 0; i < pageLen; i++) {
+            printGameCookie();
             const pageDiv = document.createElement('div');
             pageDiv.className = 'page';
-            const pageDiv2 =document.createElement('div');
-            pageDiv2.textContent = "day"+i+1;
-            pageDiv.append(pageDiv2);
-
-            const pageDiv3 =document.createElement('div');
-            pageDiv2.textContent = data.gameTime[i];
-            pageDiv.append(pageDiv3);
-            pageDiv.textContent = getPageInfo(i).text;
+            addNewElement(pageDiv, "day" + (i + 1),"h3");
+            if (data.gameTime.length > i) {
+                if (data.gameTime!=null||data.gameTime!=0) {
+                newLine(pageDiv, "game time:"+data.gameTime[i]+" hours");
+                }
+            }
+            if (data.readTime.length > i) {
+                if (data.readTime!=null||data.readTime!=0) {
+                newLine(pageDiv, "read time:"+data.readTime[i]+" hours");
+            }
+        }
+        addNewElement(pageDiv, "","br");
+            newLine(pageDiv, getPageInfo(i).text);
             $("#flipbook").append(pageDiv);
         }
 
         $("#flipbook").append(aPage());
         $("#flipbook").append(aPage());
-       
+
         // 根據頁面獲取背景顏色和文字
         $("#flipbook").turn({
             width: 400,
